667e61f7171361de1ab22006af77a2bca750992e
ewasion137 <formanban94@gmail.com>
2026-05-25 10:59:50 +0300

    updates on GUI

--- main.d
+++ main.d
@@ -1,5 +1,5 @@
 main.o: main.c api_gui.h lua/lua.h lua/luaconf.h lua/lauxlib.h \
- ../../sdk/include/stdio.h lua/lua.h lua/lua.h lua/lualib.h \
+ ../../sdk/include/stdio.h lua/lua.h lua/lualib.h \
  ../../sdk/include/equos.h ../../sdk/include/stdlib.h \
  ../../sdk/include/string.h ../../sdk/include/eid.h \
  ../../sdk/include/eid_ext.h ../../sdk/include/eid.h
@@ -8,7 +8,6 @@ lua/lua.h:
 lua/luaconf.h:
 lua/lauxlib.h:
 ../../sdk/include/stdio.h:
-lua/lua.h:
 lua/lua.h:
 lua/lualib.h:
 ../../sdk/include/equos.h:
