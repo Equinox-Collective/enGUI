01b06edc09fecfea706e22577347eaa807fa479d
ewasion137 <formanban94@gmail.com>
2026-05-31 22:20:35 +0300

    Fixed DOOM input lags and video input, also music

--- lua/lctype.d
+++
@@ -1,8 +0,0 @@
-lua/lctype.o: lua/lctype.c lua/lprefix.h lua/lctype.h lua/lua.h \
- lua/luaconf.h ../../sdk/include/limits.h lua/llimits.h
-lua/lprefix.h:
-lua/lctype.h:
-lua/lua.h:
-lua/luaconf.h:
-../../sdk/include/limits.h:
-lua/llimits.h:
--- lua/lopcodes.d
+++
@@ -1,9 +0,0 @@
-lua/lopcodes.o: lua/lopcodes.c lua/lprefix.h lua/lopcodes.h lua/llimits.h \
- ../../sdk/include/limits.h lua/lua.h lua/luaconf.h lua/lobject.h
-lua/lprefix.h:
-lua/lopcodes.h:
-lua/llimits.h:
-../../sdk/include/limits.h:
-lua/lua.h:
-lua/luaconf.h:
-lua/lobject.h:
