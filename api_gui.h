75c8a2061fe1d565cc79a70b6ed834548d20c0f0
ewasion137 <formanban94@gmail.com>
2026-06-20 18:00:32 +0300

    Cpp and imgui in enGUI

--- api_gui.h
+++ api_gui.h
@@ -1,10 +1,23 @@
 #ifndef API_GUI_H
 #define API_GUI_H
 
-#include "lua/lua.h"
+#include <stdint.h>
+#include <stdbool.h>
 
-void register_gui_api(lua_State *L);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void api_preload_boot_sound(void);
 void api_try_boot_sound(void);
+void api_tick_audio(void);
+bool play_wav_file(const char *filename);
 
+// Быстрый программный Acrylic Glass Blur
+void draw_acrylic_blur(int x, int y, int w, int h, float amount_f, int radius = 12, uint32_t tint_rgb = 0x1F222B);
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif // API_GUI_H
