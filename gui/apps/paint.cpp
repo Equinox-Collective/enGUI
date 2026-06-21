c2b5a79e59d73b2a240c1613dd9451b5fc9393c9
ewasion137 <formanban94@gmail.com>
2026-06-21 20:59:36 +0300

    15, 16/17

--- gui/apps/paint.cpp
+++ gui/apps/paint.cpp
@@ -3,48 +3,56 @@
 
 namespace GUI {
 
-    PaintApp::PaintApp() : App("Vector Paint") {
+    PaintApp::PaintApp(uint32_t id) : App("Vector Paint", id) {
         brush_color = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
-        brush_size = 4.0f;
+        brush_size = 5.0f;
         point_count = 0;
     }
 
     void PaintApp::OnRender(float dt) {
-        // Панель инструментов слева
         ImGui::BeginGroup();
         {
-            ImGui::Text("Tools");
+            ImGui::TextColored(ImVec4(1,1,1,0.6f), "PALETTE");
             ImGui::Separator();
-            ImGui::ColorEdit4("Color", (float*)&brush_color, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
-            ImGui::SliderFloat("Size", &brush_size, 1.0f, 20.0f, "%.0f");
-            if (ImGui::Button("Clear", ImVec2(50, 0))) point_count = 0;
+            
+            char uniq_picker[64];
+            sprintf(uniq_picker, "##Picker_%u", instance_id);
+            ImGui::ColorEdit4(uniq_picker, (float*)&brush_color, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
+            
+            ImGui::Spacing();
+            ImGui::Text("Size");
+            
+            char uniq_slider[64];
+            sprintf(uniq_slider, "##Slider_%u", instance_id);
+            ImGui::SliderFloat(uniq_slider, &brush_size, 1.0f, 25.0f, "%.0f px");
+            
+            ImGui::Spacing();
+            if (ImGui::Button("Clear All", ImVec2(80, 30))) point_count = 0;
         }
         ImGui::EndGroup();
 
         ImGui::SameLine();
 
-        // Холст
-        ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      // Начало холста
-        ImVec2 canvas_sz = ImGui::GetContentRegionAvail();   // Размер доступной области
-        if (canvas_sz.x < 50.0f) canvas_sz.x = 50.0f;
-        if (canvas_sz.y < 50.0f) canvas_sz.y = 50.0f;
+        // Отрисовка холста для рисования
+        ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      
+        ImVec2 canvas_sz = ImGui::GetContentRegionAvail();   
+        if (canvas_sz.x < 100.0f) canvas_sz.x = 100.0f;
+        if (canvas_sz.y < 100.0f) canvas_sz.y = 100.0f;
         ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
 
         ImDrawList* draw_list = ImGui::GetWindowDrawList();
-        draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(30, 30, 35, 255)); // Фон холста
-        draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 50));   // Рамка
+        draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(10, 12, 22, 255)); 
+        draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 30), 0.0f, 0, 1.5f);   
 
-        // Логика рисования
         ImGuiIO& io = ImGui::GetIO();
-        bool is_hovered = ImGui::IsItemHovered(); // Проверка наведения на невидимую кнопку ниже
         
-        // Создаем невидимый интерактивный элемент для захвата мыши
-        ImGui::InvisibleButton("canvas", canvas_sz);
+        char uniq_btn[64];
+        sprintf(uniq_btn, "canvas_area_%u", instance_id);
+        ImGui::InvisibleButton(uniq_btn, canvas_sz);
 
         if (ImGui::IsItemActive() && ImGui::IsMouseDown(ImGuiMouseButton_Left)) {
             ImVec2 mouse_pos_in_canvas = ImVec2(io.MousePos.x, io.MousePos.y);
             
-            // Если мы в границах холста
             if (mouse_pos_in_canvas.x >= canvas_p0.x && mouse_pos_in_canvas.x <= canvas_p1.x &&
                 mouse_pos_in_canvas.y >= canvas_p0.y && mouse_pos_in_canvas.y <= canvas_p1.y) {
                 
@@ -57,10 +65,10 @@ namespace GUI {
             }
         }
 
-        // Отрисовка всех точек (линиями для плавности)
+        // Рендеринг всех векторных точек
         for (int n = 0; n < point_count; n++) {
             draw_list->AddCircleFilled(points[n], sizes[n], colors[n]);
         }
     }
 
-} // namespace GUI
+}
