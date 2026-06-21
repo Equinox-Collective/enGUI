0a3cde26b38ac7ac815daa16631ef7f122c202d2
ewasion137 <formanban94@gmail.com>
2026-06-21 14:18:20 +0300

    api_gui.h transformation

--- api_gui.h
+++ api_gui.h
@@ -8,16 +8,36 @@
 extern "C" {
 #endif
 
+// Настройки темы
+struct Theme {
+    uint32_t accent_color;
+    uint32_t window_bg;
+    uint32_t panel_bg;
+    uint32_t text_primary;
+    uint32_t text_secondary;
+    float    blur_strength;
+    int      radius;
+};
+
+extern Theme g_CurrentTheme;
+
+// Инициализация графического ядра
+void api_gui_init();
+
+// Продвинутый блюр с эффектом зернистости (Noise) и тинтом
+void draw_acrylic_blur(int x, int y, int w, int h, float amount_f, int radius, uint32_t tint_rgb, bool draw_border = true);
+
+// Вспомогательные функции отрисовки (обертки над ядром для удобства)
+void draw_shadow(int x, int y, int w, int h, int strength);
+
+// Звуковой движок
 void api_preload_boot_sound(void);
 void api_try_boot_sound(void);
 void api_tick_audio(void);
 bool play_wav_file(const char *filename);
 
-// Быстрый программный Acrylic Glass Blur
-void draw_acrylic_blur(int x, int y, int w, int h, float amount_f, int radius = 12, uint32_t tint_rgb = 0x1F222B);
-
 #ifdef __cplusplus
 }
 #endif
 
-#endif // API_GUI_H
+#endif
